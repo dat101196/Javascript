f7e14d15fb1a4dd4f5d29f5d011c825a373ac3d0
Atom <dat101196@gmail.com>
2023-05-18 14:22:02 +0700

    Completed JS basic

--- Basic/promise.js
+++ Basic/promise.js
@@ -1,8 +1,7 @@
-//Khi promise chạy có 3 trạng thái
+//-----Khi promise chạy có 3 trạng thái
 //1.Pending: khi chưa resolve hay reject => đang gây ra memory leak
 //2.Fulfilled: khi gọi resolve
 //3.Rejected: khi gọi reject
-
 let promise = new Promise(
     //Executor
     function (resolve, reject) {
@@ -19,7 +18,6 @@ let promise = new Promise(
     }
 )
 
-
 function CallbackSuccess(result) {
     console.log('call then. Result: ', result);
 }
@@ -27,6 +25,9 @@ function CallbackSuccess(result) {
 function CallbackError(error) {
     console.log('call catch. Error: ', error);
 }
+
+
+//-----------------------------------------------------------------------
 //-----Cách hoạt động của Promise: 
 //1-promise sẽ gọi hàm then() và truyền vào 1 callback (callback thành công). Hàm callback sẽ được gọi khi executor gọi resolve(), tham số dưa vô resolve cũng chính là tham số callback nhận được
 //2-promise sẽ gọi hàm catch() và truyền vào 1 callback (callback thất bại). Hàm callback sẽ được gọi khi executor gọi reject(), tham số dưa vô reject cũng chính là tham số callback nhận được
@@ -69,8 +70,8 @@ function CallbackError(error) {
 //     console.log('finally 3');
 // })
 
-//-----Cách hoạt động của Promise: 
-//4-Khi gọi then() nhiều lần thì return của then() trước sẽ là param của then() sau.
+
+//4-Khi gọi then() nhiều lần thì return của then() trước sẽ là param của then() sau. Đây gọi là Promise chain
 //Vd:
 // promise.then(function () {
 //     console.log('then 1');
@@ -107,22 +108,74 @@ function CallbackError(error) {
 //     console.log('finally 1');
 // })
 //Vd: sleep 1s rồi mới thực hiện task lồng nhau
-function sleep(ms){
-    return new Promise(function(resolve){
+function sleep(ms) {
+    return new Promise(function (resolve) {
         setTimeout(resolve, ms);
     })
 }
 
-sleep(1000).then(() =>{
-    console.log(1);
-    return sleep(1000);
-}).then(() => {
-    console.log(2);
-    return sleep(1000);
-}).then(() => {
-    console.log(3);
-    return sleep(1000);
-}).then(() => {
-    console.log(4);
-    return sleep(1000);
-})
+// sleep(1000).then(() =>{
+//     console.log(1);
+//     return sleep(1000);
+// }).then(() => {
+//     console.log(2);
+//     return sleep(1000);
+// }).then(() => {
+//     console.log(3);
+//     return sleep(1000);
+// }).then(() => {
+//     console.log(4);
+//     return sleep(1000);
+// })
+
+
+//-----------------------------------------------------------------------
+//-----Reject
+//Khi đang chạy lần lượt các then() nếu trong 1 then khởi tạo Promise và gọi reject() thì sẽ chạy vào catch đang được hứng
+//Có thể gọi trực tiếp resolve/reject mà ko cần gán Executor(một số trường hợp return bắt buộc là promise mà ko có logic bên trong promise đó). Promise.resolve(param), Promise.reject(param)
+//Vd: 
+// sleep(1000).then(() =>{
+//     console.log(1);
+//     return sleep(1000);
+// }).then(() => {
+//     console.log(2);
+//     //return Promise.reject('Error in then 2');
+//     //return Promise.resolve('Go to log 3');
+//     return sleep(1000);
+// }).then((param) => {
+//     console.log(param || 3);
+//     return sleep(1000);
+// }).then(() => {
+//     console.log(4);
+//     return sleep(1000);
+// }).catch(function(error){
+//     console.log(error);
+// })
+
+
+//-----------------------------------------------------------------------
+//-----Promise all
+//Dùng để gọi nhiều promise chạy cùng lúc và chờ kết quả trả ra của tất cả promise được gọi mới xử lý logic tiếp trong then() sau đó. Nếu 1 trong các promise được gọi ở all() return reject thì Promise.all() sẽ chạy vô catch() chứ ko vô then(). Return của Promise.all() là 1 Promise
+//Vd:
+var promise1 = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+        resolve('Promise 1 xong.');
+        //reject('Promise 1 error');
+    }, 1000);
+});
+
+var promise2 = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+        resolve('Promise 2 xong.');
+        //reject('Promise 2 error');
+    }, 3000);
+});
+
+Promise.all([promise1, promise2]).then(result => {
+    let rs1 = result[0];
+    let rs2 = result[1];
+    console.log('result 1: ', rs1);
+    console.log('result 2: ', rs2);
+}).catch(error => {
+    console.log(error);
+});
